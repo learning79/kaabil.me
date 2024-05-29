34a6d651e4e4ecbec7c313153064e513d8b1079d
Shivam <shivamdave2903@gmail.com>
2024-05-29 16:45:40 +0530

    responsiveness

--- Frontend/src/Landing/home.jsx
+++ Frontend/src/Landing/home.jsx
@@ -10,11 +10,11 @@ import WhatWeDo from "./whatwedo";
 const Home = () => {
   return (
     <section id="home">
-    <div className="flex flex-col">
+    <div className="md:flex min-h-screen w-full flex-col">
       <Navbar />
       <div className="flex flex-col w-full h-[790px] mt-24"> 
-        <div className="flex flex-col sm:flex-row items-center"> 
-          <div className="flex-grow px-24 mr-8 sm:mr-0 mb-8 sm:mb-0"> 
+        <div className="flex flex-col md:flex-row items-center md:justify-between"> 
+          <div className="flex-grow  text-center md:text-left  py-8 px-24 mr-8 sm:mr-0 mb-8 sm:mb-0"> 
             <h1 className="text-6xl font-bold mb-4">
               AI-Powered Exam <br /> 
               <span className="whitespace-no-wrap">Preparation Platform</span>
@@ -24,8 +24,8 @@ const Home = () => {
               bite-sized lessons, you’ll earn points and unlock new levels while
               gaining real-world communication skills.
             </p>
-            <div className="flex items-center">
-              <Button className="py-8 px-12 rounded-full flex mt-16 items-center">
+            <div className="flex justify-center md:justify-start">
+              <Button className="py-8 px-12 rounded-full flex mt-16 text-center md:items-center items-center">
                 Get Started
               </Button>
             </div>
@@ -37,7 +37,7 @@ const Home = () => {
         <div className="relative w-full h-[429px]">
           <img src={banner} alt="banner"></img>
         </div>
-       <WhatWeDo />
+          <WhatWeDo />
           <HowItWorks />
       
         <Footer />
--- Frontend/src/Landing/howitworks.jsx
+++ Frontend/src/Landing/howitworks.jsx
@@ -6,13 +6,13 @@ import signup from "../assets/signup.png";
 const HowItWorks = () => {
     return (
     <section id="howItWorks">
-      <div className="bg-slate-200 w-full min-h-full font-Space Grotesk flex flex-col items-center justify-center p-8">
+      <div className="bg-slate-200 w-full min-h-full font-Space Grotesk flex flex-col md:items-center items-center md:justify-center p-8">
         <div className="align items-center"></div>
         <h1 className="text-5xl font-bold mt-16 text-center">How it Works?</h1>
         <span className="px-40 text-center mt-8">
             Kaabil.me is a self-learning platform designed to keep you engaged and motivated. Our platform tailors courses to your unique learning style, ensuring that you stay interested and make consistent progress. With Kaabil.me, you get personalized content that adapts to your needs, making learning more effective and enjoyable. The platform includes interactive quizzes and real-world projects that reinforce your knowledge. Join Kaabil.me today and experience a new way of learning that’s tailored just for you.
         </span>
-        <div className="flex flex-row mt-16 p-16 justify-center space-x-16">
+        <div className="flex flex-col md:flex-row mt-16 p-16 justify-center space-x-16">
             <div className="flex flex-col justify-center items-center text-center space-y-4">
                 <img src={signup} className="h-40 w-40" alt="signup" />
                 <Button className="rounded-full">1</Button>
