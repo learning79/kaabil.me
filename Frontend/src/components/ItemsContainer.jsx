66afaf2c1899d5938852fffc8d5478f8765eb5de
learning79 <89144131+learning79@users.noreply.github.com>
2024-06-14 04:21:37 +0530

    Update ItemsContainer

--- Frontend/src/components/ItemsContainer.jsx
+++ Frontend/src/components/ItemsContainer.jsx
@@ -2,11 +2,13 @@ import Item from "./item";
 import { PRODUCTS, RESOURCES, COMPANY, SUPPORT } from "./Menus";
 const ItemsContainer = () => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 sm:px-8 px-5 py-16">
-      <Item Links={PRODUCTS} title="PRODUCTS" />
-      <Item Links={RESOURCES} title="RESOURCES" />
-      <Item Links={COMPANY} title="COMPANY" />
-      <Item Links={SUPPORT} title="SUPPORT" />
+    <div className="flex py-16">
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-10 md:gap-60 ml-35 mr-35 mx-auto">
+        <Item Links={PRODUCTS} title="PRODUCTS" />
+        <Item Links={RESOURCES} title="RESOURCES" />
+        <Item Links={COMPANY} title="COMPANY" />
+        <Item Links={SUPPORT} title="SUPPORT" />
+      </div>
     </div>
   );
 };
