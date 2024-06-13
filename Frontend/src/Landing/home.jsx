ad21938b00c52586d870c31c205774933808e912
learning79 <89144131+learning79@users.noreply.github.com>
2024-06-13 12:33:53 +0530

    Updated home

--- Frontend/src/Landing/home.jsx
+++ Frontend/src/Landing/home.jsx
@@ -32,13 +32,14 @@ const Home = () => {
           <div className="flex flex-col md:flex-row items-center md:justify-between">
             <div className="flex-grow text-center md:px-12 md:text-left sm:mr-0 mb-8 sm:mb-0">
               <h1 className="text-6xl font-bold mb-4">
-                AI-Powered Exam <br />
-                <span className="whitespace-no-wrap">Preparation Platform</span>
+                Next - Gen <br />
+                <span className="whitespace-no-wrap">Teaching Assistant</span>
               </h1>
               <p className="text-lg mb-4">
-                Learning with Kabil is fun, and research shows that it works! With quick
-                bite-sized lessons, you’ll earn points and unlock new levels while
-                gaining real-world communication skills.
+                Meet your round-the-clock study partner!
+              </p>
+              <p className="text-lg mb-4">
+              Kaabil is here to teach you in your unique style, offer quick, bite-sized lessons, assist with last-minute preparations and track your progress all year. Earn points as you unlock your potential.
               </p>
               <div className="flex justify-center md:justify-start">
                 <Button onClick={handleGetStarted} className="py-8 px-12 rounded-full flex mt-16 text-center md:items-center items-center">
